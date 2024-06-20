b4108e5724c33e2393d828e107214cfc76431933
AmazingAndrewM1 <michaea3@tcnj.edu>
2024-06-20 08:54:34 -0400

    Add padding around the board for better viewing experience.

--- Board.java
+++ Board.java
@@ -8,10 +8,20 @@ import java.util.Random;
 public class Board extends JComponent{
     final int LENGTH;
     final int NUM_BOXES_IN_ROW;
+    final int PADDING;
+
+    public Board(){
+        this(800, 10);
+    }
 
     public Board(int length){
+        this(length, 10);
+    }
+
+    public Board(int length, int numBoxesInRow){
         this.LENGTH = length;
-        NUM_BOXES_IN_ROW = 10;
+        this.NUM_BOXES_IN_ROW = numBoxesInRow;
+        this.PADDING = 50;
     }
 
     @Override
@@ -19,11 +29,14 @@ public class Board extends JComponent{
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
         Random randGen = new Random();
-        final int BOX_SIZE = this.LENGTH / this.NUM_BOXES_IN_ROW;
+        final int BOX_SIZE = (this.LENGTH - 2 * this.PADDING) / this.NUM_BOXES_IN_ROW;
+        final int FINAL_Y_BOX = BOX_SIZE * this.NUM_BOXES_IN_ROW / 2 + this.PADDING; 
+        final int FINAL_Y = this.NUM_BOXES_IN_ROW * BOX_SIZE + this.PADDING;
+        final int FINAL_X = FINAL_Y;
 
         /* Make board rotationally symmetric */
-        for (int y = 0; y < this.LENGTH / 2; y += BOX_SIZE){
-            for (int x = 0; x < this.LENGTH; x += BOX_SIZE){
+        for (int y = this.PADDING; y < FINAL_Y_BOX; y += BOX_SIZE){
+            for (int x = this.PADDING; x < FINAL_X; x += BOX_SIZE){
                 Color color;
                 Color oppositeColor;
                 switch (randGen.nextInt(0, 5)){
@@ -46,16 +59,16 @@ public class Board extends JComponent{
                 g2D.fillRect(this.LENGTH - x - BOX_SIZE, this.LENGTH - y - BOX_SIZE, BOX_SIZE, BOX_SIZE);
             }
         }
-
+        
         g2D.setStroke(new BasicStroke(3));
         g2D.setColor(Color.GRAY);
         // Horizontal lines
-        for (int y = 0; y <= this.LENGTH; y += BOX_SIZE){
-            g2D.drawLine(0, y, this.LENGTH, y);
+        for (int y = this.PADDING; y <= FINAL_Y; y += BOX_SIZE){
+            g2D.drawLine(this.PADDING, y, this.LENGTH - this.PADDING, y);
         }
         // Vertical lines
-        for (int x = 0; x <= this.LENGTH; x += BOX_SIZE){
-            g2D.drawLine(x, 0, x, this.LENGTH);
+        for (int x = this.PADDING; x <= FINAL_X; x += BOX_SIZE){
+            g2D.drawLine(x, this.PADDING, x, this.LENGTH - this.PADDING);
         }
     }
 }
--- UserInterface.java
+++ UserInterface.java
@@ -2,14 +2,19 @@ import javax.swing.JFrame;
 
 public class UserInterface{
     private JFrame frame;
+    private int width;
+    private int height;
 
     public UserInterface(){
+        width = 800;
+        height = 800;
         frame = new JFrame();
-        frame.setTitle("Hello, World!");
+        frame.setTitle("Conway's Game of Life!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
+        frame.setSize(width, height);
+        frame.setResizable(true);
 
-        frame.add(new Board(800));
+        frame.add(new Board(width, 20));
     }
 
     public void show(){
